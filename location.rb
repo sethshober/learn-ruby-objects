2f7e7b76526ce7ddc06d3c8238b7a13096b16834
Seth Shober <sshober@newrelic.com>
2016-04-15 16:59:52 -0700

    get and create

--- location.rb
+++ location.rb
@@ -12,6 +12,10 @@ class Location
 
   attr_accessor :latitude, :longitude
 
+  attr_reader :id
+
+  attr_writer :persisted
+
   def initialize latitude:, longitude:
     @latitude = latitude
     @longitude = longitude
@@ -19,6 +23,36 @@ class Location
 
   def self.client
     @client ||= Mysql2::Client.new(:host => "localhost", :username => "root", :database => self.db_name)
+  end
+
+  def self.create latitude:, longitude:
+    new(latitude: latitude, longitude: longitude).tap do |location|
+      location.save
+    end
+  end
+
+  def self.get id
+    query = "SELECT * FROM locations WHERE id = #{id}"
+    results = client.query(query)
+    result = results.first
+    return unless results.size > 0
+    new(latitude: result['title'], longitude: result['width']).tap do |location|
+      location.persisted = true
+    end
+  end
+
+  def save
+    if persisted?
+      self.class.client.query("UPDATE locations SET latitude='#{latitude}', longitude=#{longitude} WHERE id=#{id}")
+    else
+    self.class.client.query("INSERT INTO locations(latitude, longitude) values(#{latitude}, #{longitude})")
+      @id = self.class.client.last_id
+      @persisted = true
+    end
+  end
+
+  def persisted?
+    @persisted
   end
 
 end
--- location_test.rb
+++ location_test.rb
@@ -24,6 +24,33 @@ class LocationTest < Minitest::Test
     assert_equal 75.8291, location.longitude
   end
 
+  def test_get_location
+    Location.create latitude: 45.8291, longitude: 75.8291
+    location = Location.get 1
+    assert_equal 45.8291, location.latitude
+    assert_equal 75.8291, location.longitude
+  end
+
+  def test_get_location_with_nonexistent_id
+    assert_nil Location.get 99
+  end
+
+  def test_location_create 
+    location = Location.create latitude: 45.8291, longitude: 75.8291
+    assert_equal 45.8291, location.latitude
+    assert_equal 75.8291, location.longitude
+  end
+
+  def test_location_save
+    location = Location.new
+    location.latitude = 45.8291
+    location.longitude = 75.8291
+    location.save
+    location2 = location.get location.id
+    assert_equal location2.latitude, location.latitude
+    assert_equal location2.longitude, location.longitude
+  end
+
   def clear_locations
     @client.query('TRUNCATE locations')
   end
